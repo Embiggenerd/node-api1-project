3746e765866bab6aa40f844690eb57ab64118a0d
igor <embiggenerd@gmail.com>
2020-02-09 19:15:14 -0500

    posting and displaying new user

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,12 @@ import axios from 'axios'
 
 function App() {
   const [users, setUsers] = useState({
-    user: "",
+    name: "",
     bio: "",
     userList: []
   })
+
+  const [error, setError] = useState("")
 
   useEffect(() => {
     const getUsers = async () => {
@@ -22,14 +24,40 @@ function App() {
     getUsers()
   }, [])
 
+  const handleSubmit = async event => {
+    event.preventDefault()
+    try {
+      const { data } = await axios.post('/api/users', { name: users.name, bio: users.bio })
+      const newUser = await axios.get('/api/users/'+data.id)
+      setUsers({
+        ...users,
+        userList: [...users.userList, newUser.data]
+      })
 
+    } catch (e) {
+      // setError(e.response.data.errorMessage)
+      console.dir(error)
+      setTimeout(setError(""), 3000)
+    }
+  }
+
+  const handleChange = event => {
+    setUsers({
+      ...users,
+      [event.target.name]: event.target.value
+    })
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <form
-          onSubmit={handleSubmit}></form>
+        <form onSubmit={handleSubmit}>
+          <input value={users.name} placeholder="name" name="name" onChange={handleChange} />
+          <input value={users.bio} type="textarea" placeholder="bio" name="bio" onChange={handleChange} />
+          <input type="submit" value="Submit" />
+        </form>
+        {error ? <p>{error}</p> : null}
         {users.userList.map(u => <div><h3>{u.name}</h3><p>{u.bio}</p></div>)}
       </header>
     </div>
